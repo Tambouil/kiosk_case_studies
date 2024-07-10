a61c3a0f714b9f578b0d32a85792b06409e9c588
Tambouil <v.berceaux@gmail.com>
2024-07-10 13:34:06 +0200

    chore: setup shadcn

--- senior_frontend/frontend/src/App.tsx
+++ senior_frontend/frontend/src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from './components/ui/button';
+
 function App() {
   return (
     <div className="container">
       <h1 className="container text-blue-500 underline">Hello world!</h1>
+      <Button>Button</Button>
     </div>
   );
 }
