79117dd62bbfe4e2d13e7558995e4b1e13986c03
Tambouil <v.berceaux@gmail.com>
2024-07-11 00:04:30 +0200

    refactor(granularity selector): switch to select input

--- senior_frontend/frontend/src/components/granularity-selector.tsx
+++ senior_frontend/frontend/src/components/granularity-selector.tsx
@@ -1,13 +1,4 @@
-import { DropdownMenuTrigger } from '@radix-ui/react-dropdown-menu';
-import { MixerHorizontalIcon } from '@radix-ui/react-icons';
-import { Button } from '@/components/ui/button';
-import {
-  DropdownMenu,
-  DropdownMenuCheckboxItem,
-  DropdownMenuContent,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-} from '@/components/ui/dropdown-menu';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 
 interface GranularitySelectorProps {
   value: string;
@@ -16,27 +7,14 @@ interface GranularitySelectorProps {
 
 export const GranularitySelector = ({ value, onChange }: GranularitySelectorProps) => {
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="sm" className="ml-auto flex h-8 capitalize">
-          <MixerHorizontalIcon className="mr-2 h-4 w-4" />
-          {value}
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className="w-[150px]">
-        <DropdownMenuLabel>Toggle granularity</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuCheckboxItem
-          key="monthly"
-          checked={value === 'monthly'}
-          onCheckedChange={() => onChange('monthly')}
-        >
-          Monthly
-        </DropdownMenuCheckboxItem>
-        <DropdownMenuCheckboxItem key="yearly" checked={value === 'yearly'} onCheckedChange={() => onChange('yearly')}>
-          Yearly
-        </DropdownMenuCheckboxItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Select defaultValue={value} onValueChange={onChange}>
+      <SelectTrigger className="w-[150px]">
+        <SelectValue placeholder="Toggle granularity" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="monthly">Monthly</SelectItem>
+        <SelectItem value="yearly">Yearly</SelectItem>
+      </SelectContent>
+    </Select>
   );
 };
